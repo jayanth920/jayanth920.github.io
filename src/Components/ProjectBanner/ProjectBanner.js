e47d92d147f96159b2a2e4383afc9ea089f113f1
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-05-17 01:01:59 -0500

    update

--- src/Components/ProjectBanner/ProjectBanner.js
+++ src/Components/ProjectBanner/ProjectBanner.js
@@ -3,9 +3,9 @@ import './ProjectBanner.css'
 function ProjectBanner() {
   return (
     <div className="prjBanner">
-        <div className="prjBanner_text">
+        {/* <div className="prjBanner_text"> */}
         PROJECTS
-        </div>
+        {/* </div> */}
     </div>
   )
 }
